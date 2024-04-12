cb80ab06ea9c8dde094f8d7e300c45ae5b958593
Saintkops <erwanchevrier6@gmail.com>
2024-04-12 11:47:13 +0200

    Final change before push to main

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,18 +1,32 @@
+const he = require("he");
+
 async function displayDataToHome(req, res) {
   try {
     const results = await fetch("http://127.0.0.1:3000/offers");
-    const data = await results.json();
-
-    if (!data) {
-      throw new Error("No Data found");
+    if (!results.ok) {
+      throw new Error("Failed to fetch data");
     }
 
+    const data = await results.json();
+    if (!data) {
+      throw new Error("No data found");
+    }
+
+    const decodedData = data.map((item) => {
+      const decodedItem = Object.entries(item).reduce((acc, [key, value]) => {
+        acc[key] = typeof value === "string" ? he.decode(value) : value;
+        return acc;
+      }, {});
+
+      return decodedItem;
+    });
+
     res.render("layouts/index", {
-      offers: data,
+      offers: decodedData,
     });
   } catch (err) {
     console.log(err);
-    res.status(500).send("Internal servor error !");
+    res.status(500).send("Internal server error !");
   }
 }
 
