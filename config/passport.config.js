dc9072dfbae92533f9c5eba4065d6d3c8eb7c183
Saintkops <erwanchevrier6@gmail.com>
2024-04-28 15:16:22 +0200

    commit

--- config/passport.config.js
+++ config/passport.config.js
@@ -19,7 +19,6 @@ async function initializePassport(passport) {
         default:
           throw new Error("this user type does not exist");
       }
-
   
       const query = `SELECT * FROM ${userType} WHERE email = $1`;
       const { rows } = await db.query(query, [email]);
